6f4459c40f1172871ff2fb6dd4fefafa756a4f6f
2alheure <jojo77.juv@live.fr>
2019-05-13 20:15:46 +0200

    EditButton

---
+++ src/components/writing/EditButton.vue
@@ -0,0 +1,12 @@
+<template>
+	<router-link :to="'/edit-bloc?position='+position" class="edit-button"><i class="fas fa-edit"></i></router-link>
+</template>
+
+<script>
+export default {
+	name: 'editButton',
+	props: {
+		position: String
+	}
+}
+</script>
